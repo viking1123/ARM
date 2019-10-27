6c355d07384cb1ebe3a3aaca60518765bc9fe376
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-10-27 15:37:24 +0530

    Create Q3_MT2018519.s

---
+++ Assignment1/Q3_MT2018519.s
@@ -0,0 +1,19 @@
+;ARM__Assignment-1
+;Sarvesh Nandkar : MT2018519
+;EVEN or ODD
+
+THUMB
+
+AREA     first, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main function
+		  MOV r0,#0x53	; 
+		  AND r1,r0,#0x01 ; To have only the 1st bit from lsb side of the required number  
+		  CMP r1,#0x0 	; checking if this is 0 or 1(1=odd; 0=even)
+		  ITE EQ 					
+		  MOVEQ r2,#0x0	; if R1 = 0, reflect R2 as 0 which means the number is EVEN
+		  MOVNE r2,#0x1	; if R1 = 1, reflect R2 as 1 which means  the number is ODD
+STOP		      B STOP       ; stop program 
+        endfunc
+end 
