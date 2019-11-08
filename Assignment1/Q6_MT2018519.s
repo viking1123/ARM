dc92c9cda0cfb24d1ba9414e6f214114b31ee366
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-11-08 15:08:33 +0530

    Update Q6_MT2018519.s

--- Assignment1/Q6_MT2018519.s
+++ Assignment1/Q6_MT2018519.s
@@ -6,16 +6,16 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION		 		
-		MOV  r0, #60	;input A
-		MOV  r1, #40 	;input B
+		MOV  r0, #12		;input A
+		MOV  r1, #30 		;input B
 		
-loop	CMP r0, r1		; r0 == r1?
-		BEQ stop		; YES stop
+loop	CMP r0, r1			; r0 == r1?
+		BEQ stop		; if YES, stop
 		
 		CMP r0, r1		; r0 > r1
-		ITE GT			; Works without ITE aswell
-		SUBGT r0, r1	; YES r0 -= r1
-		SUBLE r1, r0	; NO r1 -= r0
+		ITE GT			
+		SUBGT r0, r1		; if YES, r0 -= r1
+		SUBLE r1, r0		; NO r1 -= r0
 		
 		B loop
 		
