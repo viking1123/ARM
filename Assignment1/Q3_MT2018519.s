78d58a4a01d638b93210a3c4d318e79aa551959d
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-11-08 12:56:23 +0530

    Update Q3_MT2018519.s

--- Assignment1/Q3_MT2018519.s
+++ Assignment1/Q3_MT2018519.s
@@ -1,19 +1,22 @@
 ; ARM__Assignment-1
 ; Sarvesh Nandkar : MT2018519
 ; EVEN or ODD
+; output: R2
 
-THUMB
-
-AREA     first, CODE, READONLY
-     export __main	 
+     THUMB
+     AREA   first, CODE, READONLY
+     EXPORT __main
 	 ENTRY 
-__main function
-		  MOV r0,#0x53	; 
-		  AND r1,r0,#0x01 ; To have only the 1st bit from lsb side of the required number  
-		  CMP r1,#0x0 	; checking if this is 0 or 1(1=odd; 0=even)
+__main  FUNCTION
+
+		  MOV r0,#0x17		; initialize a number in R0
+		  AND r1,r0,#0x01 	; taking lsb bit, AND it with 1
+		  CMP r1,#0x0 		; checking if this bit is 0 or 1(1=odd; 0=even)
 		  ITE EQ 					
-		  MOVEQ r2,#0x0	; if R1 = 0, reflect R2 as 0 which means the number is EVEN
-		  MOVNE r2,#0x1	; if R1 = 1, reflect R2 as 1 which means  the number is ODD
-STOP		      B STOP       ; stop program 
-        endfunc
-end 
+		  MOVEQ r2,#0x0		; if R1 = 0, make R2 = 0 => number is EVEN
+		  MOVNE r2,#0x1		; if R1 = 1, make R2 = 1 => number is ODD
+
+STOP B STOP       			; stop program 
+
+ENDFUNC
+END
