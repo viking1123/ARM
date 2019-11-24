4e693ba1687cf1154188e518ca09c167110132b9
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-11-24 23:15:14 +0530

    Update MT2018519.c

--- OPEN_BOOK_EXAM/MT2018519.c
+++ OPEN_BOOK_EXAM/MT2018519.c
@@ -1,116 +1,24 @@
-
 #include "stm32f4xx.h"
 #include <string.h>
-void printMsg(const int a)
+void printMsg1(const int a)
 {
 	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg, "%x\t", a);
+	 sprintf(Msg, "%d\t", a);
 	 ptr = Msg ;
    while(*ptr != '\0'){
       ITM_SendChar(*ptr);
       ++ptr;
    }
 }
-void printMsg2p(const int a, const int b)
+
+void printMsg2(const int a)
 {
 	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg, "\n %d", a);
+	 sprintf(Msg, "%d\n", a);
 	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 sprintf(Msg, "\n %d", b);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-}
-
-void printMsg4p(const int a, const int b, const int c, const int d, const int e)
-{
-	 char Msg[100];
-	 char *ptr;
-	 // Printing the message
-	sprintf(Msg,"\nY in xy coordinate :  ");
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 //Printing the first parameter
-	 sprintf(Msg, "%d", a);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 // Printing the message
-	 sprintf(Msg,"\nX in xy coordinate : ");
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 sprintf(Msg, "%d", b);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 // Printing the message
-	 sprintf(Msg,"\nY in VGA coordinate : ");
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 sprintf(Msg, "%d", c);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 // Printing the message
-	 sprintf(Msg,"\nX in VGA coordinate : ");
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-	 sprintf(Msg, "%d", d);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-	 }
-
-	 // Printing the message
-	 //sprintf(Msg,"\nPrinting Fifth parameter e (check this value is correct or not): ");
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
-      ITM_SendChar(*ptr);
-      ++ptr;
-   }
-
-	  sprintf(Msg, "%x", e);
-	 ptr = Msg ;
-   while(*ptr != '\0')
-	 {
+   while(*ptr != '\0'){
       ITM_SendChar(*ptr);
       ++ptr;
    }
