5d7bfcb67d8f038b4419d98b7753c7d6b62672bd
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-11-26 13:22:22 +0530

    Update and rename print_msg.c to MT2018519_printmsg.c

---
+++ Assignment-3/MT2018519_printmsg.c
@@ -0,0 +1,15 @@
+#include "stm32f4xx.h"
+#include <string.h>
+#include<stdio.h>
+
+void printMsg(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
