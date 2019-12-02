13f4fac052b22cf5771c54c467adc67885e36c06
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-12-02 11:18:46 +0530

    Create MT2018519.c

---
+++ ARM_test/MT2018519.c
@@ -0,0 +1,25 @@
+#include "stm32f4xx.h"
+#include <string.h>
+void printMsg1(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "%d\t", a);
+	 ptr = Msg ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
+
+void printMsg2(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "%d\n", a);
+	 ptr = Msg ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
